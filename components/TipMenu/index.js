a778a3866c7f2eecec857962f5e370b6a807e391
Renato Siqueira <renatoelysiqueira@gmail.com>
2020-11-13 22:50:58 -0300

    Tips refact to componentize and fix text align

--- components/TipMenu/index.js
+++ components/TipMenu/index.js
@@ -1,61 +1,69 @@
-import React from 'react'
-import {makeStyles} from '@material-ui/core/styles'
-import Typography from '@material-ui/core/Typography'
-import Grid from '@material-ui/core/Grid'
-import Box from '@material-ui/core/Box'
-import Button from '@material-ui/core/Button'
-import SaveIcon from '@material-ui/icons/Save'
-import Filter2Icon from '@material-ui/icons/Filter2'
-import EmojiObjectsIcon from '@material-ui/icons/EmojiObjects'
-import EmailIcon from '@material-ui/icons/Email'
-import IconButton from '@material-ui/core/IconButton'
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Typography from "@material-ui/core/Typography";
+import Grid from "@material-ui/core/Grid";
+import Filter2Icon from "@material-ui/icons/Filter2";
+import EmojiObjectsIcon from "@material-ui/icons/EmojiObjects";
+import EmailIcon from "@material-ui/icons/Email";
+import IconButton from "@material-ui/core/IconButton";
 
 const useStyles = makeStyles(theme => ({
-    box: {
-        flex: 1
-    },
-    button: {
-        margin: theme.spacing(1),
-        borderRadius: '5em',
-        borderColor: theme.palette.secondary.main,
-        borderWidth: 1
-    }
-}))
+  box: {
+    flex: 1
+  },
+  button: {
+    margin: theme.spacing(1),
+    borderRadius: "5em",
+    borderColor: theme.palette.secondary.main,
+    borderWidth: 1
+  }
+}));
+
+const Item = ({ children, title }) => {
+  const classes = useStyles();
+  return (
+    <Grid
+      item
+      xs={4}
+      container
+      direction="column"
+      justify="center"
+      alignItems="center"
+      style={{ textAlign: "center" }}
+    >
+      <IconButton color="inherit" className={classes.button}>
+        {children}
+      </IconButton>
+      <Typography textAlign="center">{title}</Typography>
+    </Grid>
+  );
+};
 
 export default function UserPointsCard() {
-    const classes = useStyles()
+  const classes = useStyles();
 
-    return (
-        <React.Fragment>
-            <Grid spacing={1} container direction="column" justify="center" alignItems="center" className={classes.box}>
-                <Grid spacing={2} container justify="center" alignItems="flex-start">
-                    <Grid item xs={4} container direction="column" justify="center" alignItems="center">
-                        <IconButton
-                            color="inherit"
-                            className={classes.button}
-                        >
-                            <Filter2Icon fontSize="large" />
-                        </IconButton>
-                        <Typography textAlign="center">2 via de conta</Typography>
-                    </Grid>
-                    <Grid item xs={4} container direction="column" justify="center" alignItems="center">
-                        <IconButton
-                            className={classes.button}
-                        >
-                            <EmojiObjectsIcon fontSize="large" />
-                        </IconButton>
-                        <Typography textAlign="center">Comunicar falta de energia</Typography>
-                    </Grid>
-                    <Grid item xs={4} container direction="column" justify="center" alignItems="center">
-                        <IconButton
-                            className={classes.button}
-                        >
-                            <EmailIcon fontSize="large" />
-                        </IconButton>
-                        <Typography textAlign="center">Receber conta por email</Typography>
-                    </Grid>
-                </Grid>
-            </Grid>
-        </React.Fragment>
-    )
+  return (
+    <React.Fragment>
+      <Grid
+        spacing={1}
+        container
+        direction="column"
+        justify="center"
+        alignItems="center"
+        className={classes.box}
+      >
+        <Grid spacing={2} container justify="center" alignItems="flex-start">
+          <Item title="2ª Via de Conta">
+            <Filter2Icon fontSize="large" />
+          </Item>
+          <Item title="Comunicar falta de energia">
+            <EmojiObjectsIcon fontSize="large" />
+          </Item>
+          <Item title="Receber conta por email">
+            <EmailIcon fontSize="large" />
+          </Item>
+        </Grid>
+      </Grid>
+    </React.Fragment>
+  );
 }
